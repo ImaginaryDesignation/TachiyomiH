bef17dd679cee8f1663320fac56775db0e0a6839
KURIAN THOMAS <kurianthomasbaiju@gmail.com>
2023-04-19 16:08:33 +0530

    Added adjustable reader cache

--- app/src/main/java/eu/kanade/tachiyomi/data/cache/ChapterCache.kt
+++ app/src/main/java/eu/kanade/tachiyomi/data/cache/ChapterCache.kt
@@ -4,8 +4,14 @@ import android.content.Context
 import android.text.format.Formatter
 import com.jakewharton.disklrucache.DiskLruCache
 import eu.kanade.tachiyomi.source.model.Page
+import eu.kanade.tachiyomi.ui.reader.setting.ReaderPreferences
 import eu.kanade.tachiyomi.util.storage.DiskUtil
 import eu.kanade.tachiyomi.util.storage.saveTo
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
 import kotlinx.serialization.decodeFromString
 import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
@@ -39,18 +45,46 @@ class ChapterCache(private val context: Context) {
         const val PARAMETER_VALUE_COUNT = 1
 
         /** The maximum number of bytes this cache should use to store.  */
-        const val PARAMETER_CACHE_SIZE = 100L * 1024 * 1024
+//        const val PARAMETER_CACHE_SIZE = 100L * 1024 * 1024
     }
 
     private val json: Json by injectLazy()
 
     /** Cache class used for cache management.  */
-    private val diskCache = DiskLruCache.open(
-        File(context.cacheDir, PARAMETER_CACHE_DIRECTORY),
-        PARAMETER_APP_VERSION,
-        PARAMETER_VALUE_COUNT,
-        PARAMETER_CACHE_SIZE,
-    )
+    // TX -->
+    private val readerPreferences: ReaderPreferences by injectLazy()
+
+    private val scope = CoroutineScope(Job() + Dispatchers.Main)
+
+    private var diskCache = setupDiskCache(readerPreferences.cacheSize().get().toLong())
+
+    init {
+        readerPreferences.cacheSize().changes()
+            .onEach {
+                // Save old cache for destruction later
+                val oldCache = diskCache
+                diskCache = setupDiskCache(it.toLong())
+                oldCache.close()
+            }
+            .launchIn(scope)
+    }
+
+    private fun setupDiskCache(cacheSize: Long): DiskLruCache {
+        return DiskLruCache.open(
+            File(context.cacheDir, PARAMETER_CACHE_DIRECTORY),
+            PARAMETER_APP_VERSION,
+            PARAMETER_VALUE_COUNT,
+            cacheSize * 1024 * 1024,
+        )
+    }
+    // TX <--
+
+//    private val diskCache = DiskLruCache.open(
+//        File(context.cacheDir, PARAMETER_CACHE_DIRECTORY),
+//        PARAMETER_APP_VERSION,
+//        PARAMETER_VALUE_COUNT,
+//        PARAMETER_CACHE_SIZE,
+//    )
 
     /**
      * Returns directory of cache.
